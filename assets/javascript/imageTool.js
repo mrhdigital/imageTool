960526c5dc1e60c31c8c268a8ba6bdbcd08785dc
unknown <mrhdigital@yahoo.com>
2019-11-13 22:01:06 -0600

    create function the reflect the change in input values

--- assets/javascript/imageTool.js
+++ assets/javascript/imageTool.js
@@ -1,1 +1,7 @@
-alert("linked js");
+//alert("linked js");
+const inputs = document.querySelectorAll(".controls input");
+function handleUpdate() {
+    console.log(this.value);
+}
+//handleUpdate();
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
