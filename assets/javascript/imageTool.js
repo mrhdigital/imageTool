47a26092699bd62d6c286ebb5e5bdf6230b981d5
unknown <mrhdigital@yahoo.com>
2019-11-14 21:30:04 -0600

    value updates

--- assets/javascript/imageTool.js
+++ assets/javascript/imageTool.js
@@ -4,6 +4,7 @@ function handleUpdate() {
     //console.log(this.value);
     const suffix = this.dataset.sizing || '';
     console.log(this.name);
+    document.documentElement.style.setProperty(`--${this.name}`, this.value);
 }
 //handleUpdate();
 //code without arrow function:
